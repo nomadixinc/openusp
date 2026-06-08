b97a5042e9ea8111d939add916a6ab9e43b8da97
ahsclark <stuart.clark@nomadix.com>
2026-06-08 16:56:20 +0100

    fix repo name for docker

--- scripts/bash/aliases.sh
+++ scripts/bash/aliases.sh
@@ -1,3 +1,3 @@
 alias dc="docker compose -f $(pwd)/deployments/docker-compose.yaml" 
 alias dclocal="docker compose -f $(pwd)/deployments/docker-compose_local.yaml" 
-alias cli="docker run --env-file $(pwd)/configs/openusp.env --network=openusp -it --rm nomadix/openusp-cli"
+alias cli="docker run --env-file $(pwd)/configs/openusp.env --network=openusp -it --rm nomadixinc/openusp-cli"
